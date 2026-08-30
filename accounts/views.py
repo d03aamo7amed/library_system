fc82c213bda329f9933d776c06182f9c2021d4eb
d03aamo7amed <damhmddly13@gmail.com>
2026-08-30 07:51:18 +0300

    Add accounts module by Zainab

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. Please log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = 'login'
